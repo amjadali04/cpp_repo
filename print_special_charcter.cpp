f4a0b04aecb996f58df49df5b6385bd7dbe3205b
Amjad ali <amjadbhuttof@gmail.com>
2025-12-24 18:20:17 +0500

    Add files via upload

---
+++ print_special_charcter.cpp
@@ -0,0 +1,12 @@
+//To print special character:e.g: ' ,", and /.
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main(){
+	cout<<" \' "<<endl; //prints single quote(')
+	cout<<" \" "<<endl; //prints double quote(")
+	cout<<" \\ "<<endl; //prints back slash(\)
+	
+	getch();
+	return 0;
+}
